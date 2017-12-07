ba77e8eb92ead4ae1d5362d47631f9bd59c0590a
k1631133 <k1631133@kcl.ac.uk>
2017-12-07 17:43:53 +0000

    Added Post submission pages + logic

--- forum/views.py
+++ forum/views.py
@@ -3,6 +3,26 @@ from django.http import Http404
 from forum.models import Post
 from forum.models import Comment
 
+from django.utils import timezone
+from django.shortcuts import redirect
+
+from .forms import PostForm
+
+
+def post_new(request):
+   if request.method == "POST":
+      form = PostForm(request.POST)
+      if form.is_valid():
+         post = form.save(commit=False)
+         post.author = request.user
+         post.date = timezone.now()
+         post.save()
+         return redirect('post_detail', id=post.id)
+   else:
+      form = PostForm()
+   
+   return render(request,'forum/post_edit.html', {'form':form})
+
 def index(request):
    posts = Post.objects.all()
    return render(request,'forum/index.html',{
@@ -19,3 +39,5 @@ def post_detail(request,id):
       'post' : post,
       'comments' : comments
    })
+   
+
--- lab_project_razmattaz/urls.py
+++ lab_project_razmattaz/urls.py
@@ -21,5 +21,6 @@ from forum import views
 urlpatterns = [
     url(r'^$',views.index,name='index'),
     url(r'post/(?P<id>\d+)/',views.post_detail,name='post_detail'),
+    url(r'post/new/$',views.post_new,name="post_new"),
     url(r'^admin/', admin.site.urls),
 ]
