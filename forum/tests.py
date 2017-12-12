1e1085a96188e1182cb7c8c63a5584feabd0f054
k1631133 <k1631133@kcl.ac.uk>
2017-12-12 15:09:16 +0000

    Added comment submission + Post and Comment deletion + backwards naviagtion

--- forum/tests.py
+++ forum/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from .models import Post
+
+from django.utils import timezone
+
+# class ForumViewsTestCase(TestCase):
+#     def text_index(self):
+#         resp = self.client.get('/')
+#         self.assertEqual(resp.status_code,200)
+
+class PostTestCase(TestCase):
+    def setUp(self):
+        Post.objects.create(title="Post1",text="This is Post1",date=timezone.now(),author="Shakeel")
+        Post.objects.create(title="Post2",text="This is Post2",date=timezone.now(),author="Tabia")
+
+    def test_posts_created(self):
+        post1 = Post.objects.get(title="Post1")
+        post2 = Post.objects.get(title="Post2")
+        self.assertEqual(post1.__str__(),"Post1")
+        self.assertEqual(post2.__str__(),"Post2")
+        
+    def test_posts_text(self):
+        post1 = Post.objects.get(title="Post1")
+        post2 = Post.objects.get(title="Post2")
+        self.assertEquals(post1.text,"This is Post1")
+        self.assertEquals(post2.text,"This is Post2")
